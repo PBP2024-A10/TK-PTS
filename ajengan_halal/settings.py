03f084038a4ce5173b31aa32f65fca8199ab9411
DandiApriyansyah <apriyansyahdandi@gmail.com>
2024-10-27 21:04:56 +0700

    Add fitur upload udah aman, cm fitur edit blm

--- ajengan_halal/settings.py
+++ ajengan_halal/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 if DEBUG:
     STATICFILES_DIRS = [
         BASE_DIR / 'static'
@@ -136,7 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1"]
 
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
--- manajemen_souvenir/views.py
+++ manajemen_souvenir/views.py
@@ -1,16 +1,16 @@
 from django.utils.html import strip_tags
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
-from django.shortcuts import render, redirect, reverse  
+from django.shortcuts import render, redirect, reverse, get_object_or_404  
 from manajemen_souvenir.forms import SouvenirEntryForm
 from manajemen_souvenir.models import SouvenirEntry
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-import json
+import uuid, os
 
 def show_souvenir(request):
     souvenirs = SouvenirEntry.objects.all()
-    return render(request, 'souvenir.html', {'souvenirs': souvenirs})
+    return render(request, 'coba2.html', {'souvenirs': souvenirs})
 
 def show_json(request):
     data = SouvenirEntry.objects.all()
@@ -19,42 +19,59 @@ def show_json(request):
 @csrf_exempt
 @require_POST
 def add_souvenir_entry(request):
-    image = strip_tags(request.POST.get("image"))
+    image = request.FILES.get("image")
     name = strip_tags(request.POST.get("name")) # strip HTML tags!
     description = strip_tags(request.POST.get("description")) # strip HTML tags!
+    
+    if image:
+        unique_name = f"{uuid.uuid4()}_{image.name}"
+        image.name = unique_name
 
-    new_souvenir = SouvenirEntry(
-        image=image, name=name,
-        description=description
-    )
+    new_souvenir = SouvenirEntry( image=image, name=name, description=description)
     new_souvenir.save()
 
     return HttpResponse(b"CREATED", status=201)
 
-def upload_souvenir(request):
-    if request.method == 'POST':
-        form = SouvenirEntryForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('show-souvenir')  # ganti dengan redirect ke view lain
-    else:
-        form = SouvenirEntryForm()
-    return render(request, 'souvenir.html', {'form': form})
-
 def edit_souvenir(request, id):
-    # Get product entry berdasarkan id
-    souvenir = SouvenirEntry.objects.get(pk = id)
+    # Dapatkan instance souvenir berdasarkan id
+    souvenir = get_object_or_404(SouvenirEntry, pk=id)
 
-    # Set souvenir entry sebagai instance dari form
-    form = SouvenirEntryForm(request.POST or None, instance=souvenir)
+    if request.method == 'POST':
+        # Membuat form dengan data dari request
+        form = SouvenirEntryForm(request.POST, request.FILES, instance=souvenir)
 
-    if form.is_valid() and request.method == "POST":
-        # Simpan form dan kembali ke halaman awal
-        form.save()
-        return HttpResponseRedirect(reverse('show_souvenir:show_souvenir'))
+        if form.is_valid():
+            # Jika ada gambar baru
+            if 'image' in request.FILES:
+                # Simpan terlebih dahulu gambar baru ke database
+                new_image = request.FILES['image']
+                unique_name = f"{uuid.uuid4()}_{new_image.name}"
+                souvenir.image = new_image
+                souvenir.image.name = unique_name
+                
+                # Simpan form untuk menyimpan perubahan
+                form.save()
+                
+                # Hapus gambar lama setelah menyimpan gambar baru
+                if souvenir.image and os.path.isfile(souvenir.image.path):
+                    os.remove(souvenir.image.path)
 
-    context = {'form': form}
-    return render(request, "edit_souvenir.html", context)
+            else:
+                # Jika tidak ada gambar baru, tetap simpan form
+                form.save()
+            
+            # Redirect ke halaman show_souvenir setelah update
+            return redirect(reverse('show_souvenir:show_souvenir'))
+        else:
+            print("Form tidak valid:", form.errors)
+    else:
+        form = SouvenirEntryForm(instance=souvenir)
+
+    context = {
+        'form': form,
+        'souvenir': souvenir
+    }
+    return render(request, 'edit_souvenir.html', context)
 
 def delete_souvenir(request, id):
     # Get product berdasarkan id
