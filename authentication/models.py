216a22b9bf52dde125c4195b13e6557bcfeae971
olav.dendy <olavchristian517@gmail.com>
2024-10-27 20:56:09 +0700

    push wishlist 2

--- authentication/models.py
+++ authentication/models.py
@@ -1,2 +1,7 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
+
+class ProductEntry(models.Model):
+    wishlist_users = models.ManyToManyField(User, related_name='wishlist_items', blank=True)
 
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from authentication.views import login_user, logout_user, register
+from wishlist.views import  wishlist, toggle_wishlist
 
 app_name = 'authentication'
 
@@ -7,4 +8,6 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('register/', register, name='register'),
+    path('wishlist/', wishlist, name='wishlist'),
+    path('toggle_wishlist/<uuid:pk>/', toggle_wishlist, name='toggle_wishlist'),
 ]
